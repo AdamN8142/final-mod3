1e126a45579043096de3322ef3ff7c10eba2ee73
Adam Niedzwiecki <adamn8142@gmail.com>
2019-06-03 09:46:56 -0600

    Add FilterControls and PresContainer components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-
 import './App.css';
+import FilterControls from '../src/Containers/FIlterControls/FilterControls'
+import PresidentContainer from '../src/Containers/PresidentContainer/PresidentContainer'
 
 class App extends Component {
   render() {
     return (
       <div className = "App">
       <h1>Pressies and Assholes</h1>
-
+      <FilterControls />
+      <PresidentContainer />
       </div>
     )
   }
